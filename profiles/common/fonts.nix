944f23325b162e86b6911769214d7c1f97c345eb
luxus <luxuspur@gmail.com>
2022-06-29 04:21:52 +0200

    fontdir only in linux

--- profiles/common/fonts.nix
+++ profiles/common/fonts.nix
@@ -1,10 +1,12 @@
 { self, config, pkgs, lib, ... }:
-
+let
+  inherit (pkgs.stdenv) isLinux isDarwin;
+in
 {
   fonts = {
     fonts = with pkgs; [
       (nerdfonts.override { fonts = [ "Inconsolata" "IBMPlexMono" "CascadiaCode" "SourceCodePro" "FiraCode" "Hack" "Iosevka"]; })
-    ];
-    # fontDir.enable = true;
-  };
+    ] ++ (lib.optionals isLinux{ 
+     fontDir.enable = true;
+  });};
 }
