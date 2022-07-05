718ecb21ea41906fb804f12ea40422c3dd8aa4a8
luxus <luxuspur@gmail.com>
2022-07-06 00:06:35 +0200

    adding umlauts

--- profiles/nixos/wm-helper.nix
+++ profiles/nixos/wm-helper.nix
@@ -1,4 +1,19 @@
 { self, config, lib, pkgs, ... }:
+let
+  myCustomLayout = pkgs.writeText "xkb-layout" ''
+    ! Map umlauts to RIGHT ALT + <key>
+    keycode 108 = Mode_switch
+    keysym e = e E EuroSign
+    keysym c = c C cent
+    keysym a = a A adiaeresis Adiaeresis
+    keysym o = o O odiaeresis Odiaeresis
+    keysym u = u U udiaeresis Udiaeresis
+    keysym s = s S ssharp
+
+    ! disable capslock
+    ! remove Lock = Caps_Lock
+  '';
+in
 
 {
   # Graphics support
@@ -11,7 +26,7 @@
   # Theming helpers
   programs.dconf.enable = true;
   services.dbus.packages = with pkgs; [ dconf ];
-
+  services.xserver.displayManager.sessionCommands = "${pkgs.xorg.xmodmap}/bin/xmodmap ${myCustomLayout}";
   services.xrdp = {
     enable = true;
   };
